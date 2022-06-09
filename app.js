aab5f68f5f91f20d5325058a2c8f6caa0b233839
AHMAD DIDIK SETIYADI <adidiksetiyadi@gmail.com>
2022-06-09 09:20:02 +0700

    working with pug

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
-
-
 
 app.use(bodyParser.urlencoded()) // req body parser
 app.use(express.static('public')) // static files
 
 app.use('/admin', adminRouter.router)
-app.use('/shop', shopRouter)
+app.use('/', shopRouter)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile('404.html', {root: 'views'})
+    res.status(404).render('404', {pageTitle: 'Page not Found'})
 })
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,7 @@ const router = express.Router()
 const adminRouter = require('./admin')
 // /shop/ => GET
 router.get( '/', (req, res, next) => { // main page
-    // res.send("<h1> Hello from express </h1>")
-    console.log(adminRouter.products)
-    res.sendFile('shop.html', {root: 'views'})
+    res.render('shop', {products: adminRouter.products, pageTitle: 'Shop', path: '/'})
 })
 
 module.exports = router
