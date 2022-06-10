779caa1ebd2a01baf33ac14bd75ebdb8c0eec4fe
AHMAD DIDIK SETIYADI <adidiksetiyadi@gmail.com>
2022-06-10 15:16:35 +0700

    add product detail page

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,8 +7,8 @@ exports.getAddProduct = (req, res, next) => {
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
-  });
-};
+  })
+}
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
@@ -16,9 +16,9 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
-};
+  product.save()
+  res.redirect('/')
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -26,6 +26,6 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
-    });
-  });
-};
+    })
+  })
+}
--- models/product.js
+++ models/product.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
-);
+)
 
 module.exports = class Product{
-  
-  constructor(title){
+  constructor(title, imageUrl, description, price) {
       this.title = title
-      
+      this.imageUrl = imageUrl
+      this.description = description
+      this.price = price
   }
 
   // static = Static class methods are defined on the class itself.
@@ -25,23 +26,31 @@ module.exports = class Product{
     console.log(Product.path)
     fs.readFile(Product.path, (err, fileContent) => {
       if (err) {
-        cb([]);
+        cb([])
       } else {
-        cb(JSON.parse(fileContent));
+        cb(JSON.parse(fileContent))
       }
     })
   }
 
   save() {
-      Product.getProductsFromFile(products => {
-        products.push(this);
-        fs.writeFile(Product.path, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      });
-    }
+    this.id = Math.random().toString()
+    Product.getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(Product.path, JSON.stringify(products), err => {
+        console.log(err);
+      })
+    })
+  }
 
   static fetchAll(cb) {
-    Product.getProductsFromFile(cb);
+    Product.getProductsFromFile(cb)
+  }
+
+  static findById(id, cb) {
+    Product.getProductsFromFile(products => {
+      const product = products.find(p => p.id === id)
+      cb(product)
+    })
   }
 }
